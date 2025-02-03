b84ad4f53ac56da34db75626ff99aae15be71cc6
Elton0000 <nhanelton@gmail.com>
2025-02-02 20:21:33 -0800

    Test(2)

---
+++ Files/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        PlayerCharacter Elton = new PlayerCharacter();
+        Elton.levelUp();
+        Elton.spellList.get(0).getSpellDescription();
+        }
+    }
