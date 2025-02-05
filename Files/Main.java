2fc1fa92c5695891e677e96123cb185687c15841
Elton0000 <nhanelton@gmail.com>
2025-02-04 20:33:00 -0800

    removed Files folder

--- Files/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        PlayerCharacter Elton = new PlayerCharacter();
-        Elton.levelUp();
-        Elton.spellList.get(0).getSpellDescription();
-        }
-    }
