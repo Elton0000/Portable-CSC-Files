770a3412b75bb1a6484d448baaa8c2ea98299711
Elton0000 <nhanelton@gmail.com>
2025-02-04 20:25:34 -0800

    some Input Output stuff

---
+++ Assignment-02-Code (2)/StdOutStdErrTee.java
@@ -0,0 +1,56 @@
+/*
+ * **********************************************
+ * San Francisco State University
+ * CSC 220 -  Data Structures
+ * File Name: StdOutStdErrTee.java
+ * Author: Duc Ta
+ * Author: <First Name> <Last Name>
+ * **********************************************
+ */
+
+package assignment02PartB;
+import java.io.IOException;
+import java.io.OutputStream;
+// Please organize all the given files in 1 same package
+// Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
+
+
+
+public class StdOutStdErrTee extends OutputStream {
+
+    //
+    // Static Data Fields
+    //
+
+    //
+    // Instance Data Fields
+    //
+
+    //
+    // Constructors
+    //
+    public StdOutStdErrTee() {
+
+    }
+    //
+    // Instance Methods
+    //
+
+    //
+    // Additional Methods
+    //
+    public static void startLog() {
+
+    }
+    //
+    // Language
+    //
+
+    //
+    // Override
+    @Override
+    public void write(int b) throws IOException {
+
+    }
+    //
+}
