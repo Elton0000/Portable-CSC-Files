878aef3c771bc01b8bd90db4be3000dd454e44f5
Elton0000 <nhanelton@gmail.com>
2025-03-19 07:07:45 -0700

    LinkedBag Started

--- Assignment-03-Code/assignment03PartA/LinkedBag.java
+++ Assignment-03-Code/assignment03PartA/LinkedBag.java
@@ -21,9 +21,82 @@ public final class LinkedBag<T> implements PrimaryDataStructureBagInterface<T> {
         firstNode = null;
         numberOfEntries = 0;
     }
-
     @Override
-    public boolean removeAllOccurrences(T[][] entries) {
+    public int getCurrentSize(){
+        return numberOfEntries;
+    }
+    @Override
+    public boolean isEmpty(){
+        return numberOfEntries == 0;
+    }
+    @Override
+    public boolean add(T newEntry){
+        boolean success;
+        Node newNode = new Node(newEntry);
+        newNode.next = firstNode;
+        firstNode = newNode;
+        numberOfEntries++;
+        success = true;
+        return success;
+    }
+    @Override
+    public boolean removeAllOccurrences(T[][] entries){
+        System.out.print("[-] Converting 2D array to 1D...\n");
+
+        int totalSize = 0;
+        for (Object [] row : entries) {
+            totalSize += row.length;
+        }
+        int index = 0;
+        T [] convertedArray = (T []) new Object [totalSize];
+        for (Object [] row : entries) {
+            for (Object col : row) {
+                convertedArray[index] = (T) col;
+                index++;
+            }
+        }
+
+        System.out.print("[-] Removing duplicates in 1D array...\n");
+
+        int subtractFromSize = 0;
+        for (int i = 0; i < convertedArray.length - 1; i++) {
+            for (int j = i + 1; j < convertedArray.length; j++) {
+                if (convertedArray[i] != null && convertedArray[i] == convertedArray[j]) {
+                    convertedArray[j] = null;
+                    subtractFromSize++;
+                }
+            }
+        }
+
+        T [] convertedArray2 = (T[]) new Object[totalSize - subtractFromSize];
+        index = 0;
+        for (T oldNonNull : convertedArray) {
+            if (oldNonNull != null) {
+                convertedArray2[index] = oldNonNull;
+                index++;
+            }
+        }
+
+        System.out.print("[>] The final 1D array now contains: ");
+        for (T nonDuplicate : convertedArray2) {
+            System.out.print(nonDuplicate + " ");
+        }
+        System.out.print("\n[-] Removing the final 1D array items from the bag...\n");
+
+        return false;
+    }
+    @Override
+    public T[] toArray(){
+        T [] myArray = (T[]) new Object[getCurrentSize()];
+        Node currentNode = firstNode;
+        int index = 0;
+
+        while (currentNode != null && index < numberOfEntries) {
+            myArray[index] = currentNode.data;
+            currentNode = currentNode.next;
+            index++;
+        }
+        return myArray;
     }
 
     private class Node {
