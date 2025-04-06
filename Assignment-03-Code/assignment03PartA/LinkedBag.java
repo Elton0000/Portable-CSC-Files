c774be6d6b4ac984bd9efa960e3de263ed5fd288
Elton0000 <nhanelton@gmail.com>
2025-04-05 19:08:32 -0700

    final before asmt 4

--- Assignment-03-Code/assignment03PartA/LinkedBag.java
+++ Assignment-03-Code/assignment03PartA/LinkedBag.java
@@ -31,25 +31,40 @@ public final class LinkedBag<T> implements PrimaryDataStructureBagInterface<T> {
     }
     @Override
     public boolean add(T newEntry){
-        boolean success;
+        boolean success = false;
+
         Node newNode = new Node(newEntry);
         newNode.next = firstNode;
         firstNode = newNode;
         numberOfEntries++;
+
         success = true;
         return success;
     }
+    private int forOne (T[][] entries, int startingIndex) {
+        int sum = 0;
+        if (startingIndex >= entries.length) {
+            return 0;
+        }
+        else {
+            sum += forOne(entries,startingIndex + 1) + entries[startingIndex].length;
+        }
+        return sum;
+    }
+//    private T[] forTwo(T[][] entries, int startingIndex, int totalSize ) {
+//
+//    }
     @Override
     public boolean removeAllOccurrences(T[][] entries){
         System.out.print("[-] Converting 2D array to 1D...\n");
 
-        int totalSize = 0;
-        for (Object [] row : entries) {
-            totalSize += row.length;
-        }
+        int totalSize = forOne(entries,0); //1
+
         int index = 0;
         T [] convertedArray = (T []) new Object [totalSize];
-        for (Object [] row : entries) {
+        for (Object [] row : entries) { //2
+            //A new 1D array has been created, the task is now to take every element
+            //In each row left to right, then top to down, and put into a 1D array
             for (Object col : row) {
                 convertedArray[index] = (T) col;
                 index++;
@@ -59,7 +74,7 @@ public final class LinkedBag<T> implements PrimaryDataStructureBagInterface<T> {
         System.out.print("[-] Removing duplicates in 1D array...\n");
 
         int subtractFromSize = 0;
-        for (int i = 0; i < convertedArray.length - 1; i++) {
+        for (int i = 0; i < convertedArray.length - 1; i++) { //3
             for (int j = i + 1; j < convertedArray.length; j++) {
                 if (convertedArray[i] != null && convertedArray[i] == convertedArray[j]) {
                     convertedArray[j] = null;
@@ -70,15 +85,15 @@ public final class LinkedBag<T> implements PrimaryDataStructureBagInterface<T> {
 
         T [] convertedArray2 = (T[]) new Object[totalSize - subtractFromSize];
         index = 0;
-        for (T oldNonNull : convertedArray) {
-            if (oldNonNull != null) {
+        for (T oldNonNull : convertedArray) { //4
+            if (oldNonNull != null) { //if the entry is not null, add it to the first open index of convertedArray2
                 convertedArray2[index] = oldNonNull;
                 index++;
             }
         }
 
         System.out.print("[>] The final 1D array now contains: ");
-        for (T nonDuplicate : convertedArray2) {
+        for (T nonDuplicate : convertedArray2) { //5
             System.out.print(nonDuplicate + " ");
         }
         System.out.print("\n[-] Removing the final 1D array items from the bag...\n");
