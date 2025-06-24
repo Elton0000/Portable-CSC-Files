f390b8e09bba4fd68821811a87baabcc0fcc640a
Elton0000 <nhanelton@gmail.com>
2025-06-24 15:16:32 -0700

    asmt 3

--- Dictionary App/src/BagInterface.java
+++
@@ -1,8 +0,0 @@
-public interface BagInterface <T>{
- boolean contains(T entry);
- int getDefinitionTotal ();
- boolean add(T entry);
- void printList(String word);
- void printList(String word, boolean isDistinct, boolean isReversed);
- boolean isEmpty();
-}
