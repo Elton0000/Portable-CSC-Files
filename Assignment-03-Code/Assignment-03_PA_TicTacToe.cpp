a827fe36de2be3a2b17f33f97610e485962edb59
Elton0000 <nhanelton@gmail.com>
2025-06-29 18:21:26 -0700

    partA mostly done, need isWon method

--- Assignment-03-Code/Assignment-03_PA_TicTacToe.cpp
+++ Assignment-03-Code/Assignment-03_PA_TicTacToe.cpp
@@ -1,17 +1,69 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-bool isWon(char, char[][3]) {
-	return false;
-}
-bool isDraw(char[][3]) {
-	return false;
-}
-void displayBoard(char[][3]) {
-	cout << "hello";
-}
-void makeAMove(char[][3], char) {
+bool isWon(char input, char board[][3]) {
+	//O = 79 // X = 88  // 5X, 4O = 756
+	bool won = false;
+	
+	
+	
 
+	return won;
+}
+
+bool isDraw(char board[][3]) {
+	int total = 0;
+	for (int i = 0; i < sizeof(*board); i++) {//row 0 -> 3 transition
+		for (int j = 0; j < sizeof(board[i]); j++) {//column to column 0 ->3 // top left to bottom right
+			if (board[i][j] != ' ') {
+				total += board[i][j];
+			}
+		}
+	}
+	return total == 747 || total == 756; //sum of combination of X and Os
+}
+
+void displayBoard(char board[][3]) {
+	string lines = "-------";
+	
+	for (int i = 0; i < sizeof(*board); i++) {//row 0 -> 3 transition
+		cout << lines << "\n";
+		for (int j = 0; j < sizeof(board[i]); j++) {//column to column 0 ->3 // top left to bottom right
+			cout << "|" << board[i][j];
+		}
+		cout << "|\n";
+	}
+	cout << lines << "\n";
+}
+
+void makeAMove(char board[][3], char currentPlayer) {
+	int input[2];
+	cout << board[0];
+	cout << "Enter a row (0, 1, 2) for player " << currentPlayer << ": ";
+
+	while (!(cin >> input[0]) || (input[0] != 0 && input[0] != 1 && input[0] != 2)) {
+		cin.clear();
+		cin.ignore();
+		cout << "Incorrect input. Please try again.\n";
+		cout << "Enter a row (0, 1, 2) for player " << currentPlayer << ": ";
+	}
+
+	cout << "Enter a column (0, 1, 2) for player " << currentPlayer << ": ";
+	while (!(cin >> input[1]) || (input[1] != 0 && input[1] != 1 && input[1] != 2)) {
+		cin.clear();
+		cin.ignore();
+		cout << "Incorrect input. Please try again.\n";
+		cout << "Enter a column (0, 1, 2) for player " << currentPlayer << ": ";
+	}
+
+	if (board[input[0]][input[1]] != ' ') {
+		cout << "This cell is already occupied. Try a different cell.\n";
+			makeAMove(board, currentPlayer);
+	}
+	else {
+		board[input[0]][input[1]] = currentPlayer;
+	}
 }
 
 int main() {
@@ -20,13 +72,13 @@ int main() {
 	//
 	char board[3][3] = { { ' ', ' ', ' ' },{ ' ', ' ', ' ' },{ ' ', ' ', ' ' } };
 	displayBoard(board);
-
+	
 	while (true) {
 
 		// The first player makes a move
 		makeAMove(board, 'X');
 		displayBoard(board);
-
+		
 		if (isWon('X', board)) {
 			cout << "X player won" << endl;
 			exit(0);
@@ -48,7 +100,8 @@ int main() {
 			cout << "No winner" << endl;
 			exit(0);
 		}
+		
 	}
 
 	return 0;
-} 
+}
